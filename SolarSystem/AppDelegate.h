30125f26800e22922d59a07909bc040bfc1cdc9e
Joshua Kyle Rodgers <jkr332@mcintosh.cs.mun.ca>
2015-03-10 10:19:35 -0230

    Initial Commit

---
+++ SolarSystem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SolarSystem
+//
+//  Created by Joshua Kyle Rodgers on 2015-03-10.
+//  Copyright (c) 2015 Joshua Kyle Rodgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SolarSystem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SolarSystem
+//
+//  Created by Joshua Kyle Rodgers on 2015-03-10.
+//  Copyright (c) 2015 Joshua Kyle Rodgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
